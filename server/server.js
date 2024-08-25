df903689e04d5c9bad1a63d65a2fed6c7a7fefc9
donexinfotech <donexinfotech@gmail.com>
2024-08-25 13:22:54 +0530

    added frontend link

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const adminRoute = require("./router/admin-router");
 
 const corsOptions = {
     // origin :"http://localhost:5173",
-    origin :"https://portfolio-api-gilt.vercel.app",
+    origin :"https://portfolio-client-kohl.vercel.app/",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials:true,
 }
